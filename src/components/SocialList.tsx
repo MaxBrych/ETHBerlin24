5fe9d444ef3474cc395b4715f1b1b26b828e9bfc
MaxBrych <max.brych03@gmail.com>
2024-05-24 20:16:15 +0200

    inital airstack query test

---
+++ src/components/SocialList.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { init, useQuery } from "@airstack/airstack-react";
+import React from "react";
+
+if (typeof window !== "undefined") {
+  init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY as string);
+}
+
+const query = `{
+  Socials(
+    input: {filter: {profileName: {_regex_in: ["^a", "^lens/@a"]}}, blockchain: ethereum}
+  ) {
+    Social {
+      dappName
+      profileName
+    }
+  }
+}`;
+
+export const SocialList = () => {
+  const { data, loading, error } = useQuery(query);
+
+  console.log("Query:", query);
+  console.log("Data:", data);
+  console.log("Loading:", loading);
+  console.log("Error:", error);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        <h2>Query Results:</h2>
+        <ul>
+          {data.Socials.Social.map((item: any, index: number) => (
+            <li key={index}>
+              <strong>Dapp Name:</strong> {item.dappName} <br />
+              <strong>Profile Name:</strong> {item.profileName}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  return null;
+};
